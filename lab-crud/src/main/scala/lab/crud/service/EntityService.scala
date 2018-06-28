16087a72547cc23fd7737f99e306f016c8472a76
ZhouXunYou <zhouxy@zctt.com>
2018-06-28 20:26:02 +0800

    注释

--- lab-crud/src/main/scala/lab/crud/service/EntityService.scala
+++ lab-crud/src/main/scala/lab/crud/service/EntityService.scala
@@ -12,21 +12,7 @@ import org.springframework.data.domain.Page
 import lab.crud.suport.Pagination
 
 trait EntityService[Entity <: EntityBean, Repository <: EntityRepository[Entity]] {
-  /**
-   * public Serializable add(Entity entity);
-   * public Set<Serializable> add(Iterable<Entity> entitys);
-   * public void remove(Entity entity);
-   * public void remove(String id);
-   * public void remove(Iterable<Entity> entitys);
-   * public void remove(Set<String> ids,Class<?> entityClass);
-   * public void modify(Entity entity);
-   * public Entity get(String id);
-   * public Page<Entity> find(QueryPage queryPage);
-   * public Repository getRepository();
-   * public Object invokeMethod(String methodName,Class<?>[] paramTypes,Object...params) throws RuntimeException;
-   * public Entity saveOrUpdate(Entity entity);
-   *
-   */
+  
   /**
    * 新增或修改
    */
@@ -43,18 +29,34 @@ trait EntityService[Entity <: EntityBean, Repository <: EntityRepository[Entity]
    * 用ID删除
    */
   def remove(id: Serializable): Unit
-  
+  /**
+   * 通过ID批量删除
+   */
   def batchRemove(ids: Array[Serializable]): Unit
-
+  /**
+   * 通过实体批量删除
+   */
   def batchRemove(entities: JavaIterable[Entity]): Unit
 
   /**
-   * 通过ID获取
+   * 通过ID获取实体
    */
   def get(id: Serializable): Entity
-
+  
+  /**
+   * 条件查询
+   */
   def find(fieldConditions: JavaList[FieldCondition]): JavaList[Entity]
+  /**
+   * 条件查询、排序
+   */
   def find(fieldConditions: JavaList[FieldCondition], sorts: Array[Sortor]): JavaList[Entity]
+  /**
+   * 条件查询、分页
+   */
   def find(fieldConditions: JavaList[FieldCondition], pagination: Pagination): Page[Entity]
+  /**
+   * 条件查询、分页、排序
+   */
   def find(fieldConditions: JavaList[FieldCondition], pagination: Pagination, sorts: Array[Sortor]): Page[Entity]
 }
